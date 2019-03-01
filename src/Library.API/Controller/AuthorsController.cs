b7574a6756d8b658c7ae041dbb3e4c3c4bc662e9
selen çakıroğlu <selencakiroglu@gmail.com>
2019-03-01 22:47:21 +0300

    Add project files.

---
+++ src/Library.API/Controller/AuthorsController.cs
@@ -0,0 +1,47 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Helpers;
+using Library.API.Models;
+using AutoMapper;
+
+namespace Library.API.Controller
+{
+    [Route("api/authors")]
+
+    public class AuthorsController : ControllerBase
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+                var authorsFromRepo = _libraryRepository.GetAuthors();
+
+                var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+                return Ok(authors);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var authorFromRepo = _libraryRepository.GetAuthor(id);
+
+            if(authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            var author = Mapper.Map<AuthorDto>(authorFromRepo);
+            return Ok(author);
+        }
+    }
+}
---
+++ src/Library.API/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Library.API.Services;
+using Library.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Library.API.Helpers;
+
+namespace Library.API
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+       public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+           var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            ILoggerFactory loggerFactory, LibraryContext libraryContext)
+        {           
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again alter.");
+                    });
+                });
+            }
+
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Entities.Author, Models.AuthorDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src =>
+                $"{src.FirstName} {src.LastName}"))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
+                src.DateOfBirth.GetCurrentAge()));
+
+                cfg.CreateMap<Entities.Book, Models.BookDto>();
+            });
+
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseMvc(); 
+        }
+    }
+}
