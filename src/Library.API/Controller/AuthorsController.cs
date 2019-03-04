78f2df01f090b5f06a2a8357e183878e60fd8b3e
selen çakıroğlu <selencakiroglu@gmail.com>
2019-03-04 17:05:10 +0300

    Added delete method in AuthorsController.

--- src/Library.API/Controller/AuthorsController.cs
+++ src/Library.API/Controller/AuthorsController.cs
@@ -80,5 +80,24 @@ namespace Library.API.Controller
 
             return NotFound();
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(Guid id)
+        {
+            var authorFromRepo = _libraryRepository.GetAuthor(id);
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _libraryRepository.DeleteAuthor(authorFromRepo);
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception($"Deleting author {id} failed on save.");
+            }
+
+            return NoContent();
+        }
     }
 }
