6c00f5316136aa34eb8ac3d70172017e874339e2
selen çakıroğlu <selencakiroglu@gmail.com>
2019-03-06 02:09:45 +0300

    Added BookForUpdateDto in Models, added put method for update resources in BooksControler, and added mapping models to entities

--- src/Library.API/Controller/BooksController.cs
+++ src/Library.API/Controller/BooksController.cs
@@ -108,5 +108,37 @@ namespace Library.API.Controller
 
             return NoContent();
         }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBookForAuthor(Guid authorId, Guid id,
+            [FromBody] BookForUpdateDto book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if (bookForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            Mapper.Map(book, bookForAuthorFromRepo);
+
+            _libraryRepository.UpdateBookForAuthor(bookForAuthorFromRepo);
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception($"Updating book {id} for author {authorId} failed on save.");
+            }
+
+            return NoContent();
+        }
     }
 }
--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -78,6 +78,8 @@ namespace Library.API
                 cfg.CreateMap<Models.AuthorForCreationDto, Entities.Author>();
 
                 cfg.CreateMap<Models.BookForCreationDto, Entities.Book>();
+
+                cfg.CreateMap<Models.BookForUpdateDto, Entities.Book>();
             });
 
             libraryContext.EnsureSeedDataForContext();
